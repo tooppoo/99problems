8d76b2649a582166b3c4c802573ffc895ec904e1
philomagi <tooppoo56@gmail.com>
2021-01-30 12:02:47 +0900

    implement problem generator

---
+++ haskell/app/AddProblem.hs
@@ -0,0 +1,30 @@
+module Main (main) where
+
+import System.Environment (getArgs)
+import Text.Printf
+
+main :: IO ()
+main = do
+  args <- getArgs
+
+  let strN = args !! 0
+      n = read strN :: Int
+  writeFile ("./src/Problems/Problem%03d.hs" <| n) (problem n)
+  writeFile ("./test/Problems/Problem%03dSpec.hs" <| n) (spec n)
+
+problem :: Int -> String
+problem n = "module Problems.Problem%03d where" <| n
+
+spec :: Int -> String
+spec num = concat [
+    "module Problems.Problem%03dSpec where\n" <| num,
+    "\n",
+    "import Test.Hspec\n",
+    "import Problems.Problem%03d\n" <| num,
+    "\n",
+    "spec = do\n",
+    "  describe \"Problem%03d\" $ do" <| num
+  ]
+
+(<|) :: String -> Int -> String
+(<|) template index = printf template index
